e81469edfb056e600a192c410c157cb2cf9994e0
sanchit <sanchitkumar0307@gmail.com>
2024-04-04 13:56:04 +0530

    added

---
+++ vars/deployRedis.groovy
@@ -0,0 +1,21 @@
+def call() {
+    // Clone the repository
+    git url: 'https://github.com/Sanchit2323/Test123.git'
+
+    // User Approval
+    input message: 'Approve the deployment?', submitter: 'admin,sanchit'
+
+    // Playbook Execution
+    ansiblePlaybook(
+        credentialsId: '26c1d4a1-4584-469b-a8af-8f5d84ef3842',
+        inventory: '/home/sanchit/ansible3/hosts',
+        playbook: 'home/sanchit/ansible3/redis.yml'
+    )
+
+    // Notification
+    emailext(
+        subject: 'Ansible Deployment Status',
+        body: 'The Ansible deployment has completed.',
+        to: 'sanchitkumar0307@gmail.com'
+    )
+}
